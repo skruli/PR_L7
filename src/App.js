62bb6233d8e2a53d58fca769ef0c960c78983981
skruli_ <grzesiek.789123@gmail.com>
2021-04-30 12:14:09 +0200

    Route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 import Posts from "./components/posts";
+import NavBar from "./components/navbar";
+import NotFound from "./components/notFound";
+import { Route, Switch } from "react-router-dom";
+import Home from "./components/home";
 
 function App() {
   return (
-    <main className="container">
-             <Posts/>
-         </main>
+    <div className="container-fluid">
+           <NavBar />
+           <div className="container">
+               <div className="content">
+                   <Switch>
+                       <Route path="/posts" component={Posts} />}
+                       />
+                       <Route path="/" exact component={Home} />
+                       <Route component={NotFound}/>
+                   </Switch>
+               </div>
+           </div>
+         </div>
 
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
 
 ReactDOM.render(
+<BrowserRouter>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+</BrowserRouter>,
   document.getElementById('root')
 );
 
